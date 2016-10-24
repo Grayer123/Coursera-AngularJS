50ec8f63a88b66a22196adff475628e5d25f5a5b
Grayer <ranwang568@gmail.com>
2016-10-24 14:57:22 -0400

    Initial Complete

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# Coursera-AngularJS
-Repository for Coursera Single Page Web Application with AngularJS
+(function(){
+  'use strict'
+
+  angular.module('ShoppingListCheckOffApp', [])
+    .controller('LunchCheckController', CheckController);
+
+    CheckController.$inject = ['$scope'];
+    function CheckController($scope){
+      $scope.lunch = '';
+      $scope.checkComma = function(){
+        var str = $scope.lunch;
+        setColor($scope, str);
+        setMsg($scope, str);
+      }
+    }
+
+    
+    function CheckOffServiceProvider(){
+        var provider = this;
+		
+		provider.defaults = {
+			maxItems : 5
+		};
+
+        provider.$get = function(){
+            var checkOff = new CheckOffService(provider.defaults.maxItems);
+
+            return checkOff;
+        };
+    }
+
+})();
--- module2-solution/app.js
+++ module2-solution/app.js
@@ -2,53 +2,109 @@
   'use strict'
 
   angular.module('ShoppingListCheckOffApp', [])
-    .controller('LunchCheckController', CheckController);
+    .controller('ToBuyController', ToBuyController)
+    .controller('AlreadyBoughtController', AlreadyBoughtController)
+    .provider('CheckOffService', CheckOffServiceProvider);
+    // .config(Config);
 
-    CheckController.$inject = ['$scope'];
-    function CheckController($scope){
-      $scope.lunch = '';
-      $scope.checkComma = function(){
-        var str = $scope.lunch;
-        setColor($scope, str);
-        setMsg($scope, str);
+    // Config.$inject = ['ShoppingListServiceProvider'];
+    // function Config(ShoppingListServiceProvider) {
+    //   // Save Yaakov from himself
+    //   ShoppingListServiceProvider.defaults.maxItems = 2;
+    // }
+
+    //ToBuy Controller
+    ToBuyController.$inject = ['CheckOffService'];
+    function ToBuyController(CheckOffService){
+      var toBuyList = this;
+
+      toBuyList.items = CheckOffService.getToBuyItems();
+
+      toBuyList.checkOffItem = function(itemIndex){
+        CheckOffService.checkOffItem(itemIndex);
+      }
+
+      toBuyList.checkIfEmpty = function(){
+        return CheckOffService.checkIfToBuyEmpty();
       }
     }
 
 
-    function setColor($scope, str){
-      if(str.length === 0){
-        $scope.fontColor={color:'red'};
-        $scope.borderColr={'border-color':'red'};
-        return;
+    //Already Bought Controller
+    AlreadyBoughtController.$inject = ['CheckOffService'];
+    function AlreadyBoughtController(CheckOffService){
+      var boughtList = this;
+
+      boughtList.items = CheckOffService.getBoughtItems();
+
+      boughtList.checkIfEmpty = function(){
+        return CheckOffService.checkIfBoughtEmpty();
       }
-      $scope.fontColor = {color:'green'};
-      $scope.borderColr={'border-color':'green'};
     }
 
+    function CheckOffService(){
+      var service = this;
 
-    function setMsg($scope, str){
-      if(str.length === 0){
-        $scope.msg = 'Please enter data first';
-        return;
+      //List of to buy items
+      var toBuyItems = [
+        { name: "cookies",
+          quantity: 10
+        },
+        { name: "Peas",
+          quantity: 5
+        },
+        { name: "Almond",
+          quantity: 3
+        },
+        { name: "Egg-tart",
+          quantity: 5
+        },
+        { name: "pistachio",
+          quantity: 2
+        },
+        { name: "Banana",
+          quantity: 8
+        }];
+
+      //List of bought items
+      var boughtItems = [];
+
+      service.getToBuyItems = function(){
+        return toBuyItems;
       }
-      var cnt = count(str);
-      if(cnt < 4){
-        $scope.msg = 'Enjoy!';
-        return;
+
+      service.getBoughtItems = function(){
+        return boughtItems;
       }
-      $scope.msg = 'Too much!';
+
+      service.checkOffItem = function(itemIndex){
+        var item = (toBuyItems.splice(itemIndex, 1))[0];
+        boughtItems.push(item);
+      }
+
+      //true for not empty;
+      service.checkIfToBuyEmpty = function(){
+        return toBuyItems.length > 0;
+      }
+
+      //true for not empty;
+      service.checkIfBoughtEmpty = function(){
+        return boughtItems.length > 0;
+      }
     }
 
+    function CheckOffServiceProvider(){
+      var provider = this;
 
-   function count(str){
-     var arr = str.split(',');
-     var cnt = 0;
-     for (var item of arr) {
-       if ((item.trim()).length !== 0) {
-         cnt++;
-       }
-     }
-     return cnt;
-   }
+  		// provider.defaults = {
+  		// 	maxItems : 5
+  		// };
+
+      provider.$get = function(){
+          var checkOff = new CheckOffService();
+
+          return checkOff;
+      };
+  }
 
 })();
